b99908df4d0f76c4c39a9e16d28260c6f35c83d8
Travis CI <builds@travis-ci.com>
2020-01-14 23:46:35 -0500

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/LimeEvo1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LimeEvo1.dir/main.cpp.o"
+  "LimeEvo1"
+  "LimeEvo1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LimeEvo1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
